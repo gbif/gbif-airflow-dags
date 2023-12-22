0a05aa288ee45ec06240a8aafd55aecd5fe0ff88
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2023-12-22 08:54:43 +0100

    grscicoll cache

--- dags/gbif-grscicoll-cache.py
+++ dags/gbif-grscicoll-cache.py
@@ -42,7 +42,8 @@ with DAG(
       trino_conn_id='gbif_trino_conn',
       sql="""USE hive;DROP TABLE IF EXISTS marcos.occurrence_collections;
            CREATE TABLE marcos.occurrence_collections WITH (format = 'PARQUET') AS
-           SELECT DISTINCT ownerInstitutionCode, institutionId, institutionCode, collectionCode, collectionId, datasetKey,
+           SELECT DISTINCT ownerInstitutionCode, institutionId, institutionCode, collectionCode,
+           collectionId, datasetKey,
            CASE WHEN datasetKey = '4fa7b334-ce0d-4e88-aaae-2e0c138d049e' THEN 'US' ELSE publishingCountry END AS country
            FROM dev2.occurrence;""")
 
